12aeada5196d6e234912d688acf3a646f9286da2
Anzana Sharma <anzana.sharma@gmail.com>
2018-02-08 21:45:47 +0545

    array solution set

--- js/array.js
+++ js/array.js
@@ -1,7 +1,36 @@
 //print reverse
-var reverse = ['1','2','3','4','5'];
-function printReverse() {
-  for (var i = 0; i < reverse.length; i++) {
-    reverse[i]
+function printReverse(arr) {
+  for (var i = arr.length-1; i >= 0; i--) {
+    console.log(arr[i]);
   }
+}
+printReverse([1,2,3,4,5]);
+
+// isUniform()
+function isUniform(arr){
+  var first = arr[0];
+  for(var i = 1; i<arr.length; i++) {
+    if (arr[i] !== first) {
+      return false;
+    }
+  }
+  return true;
+}
+//sum
+function sumArray(arr){
+  var total = 0;
+  arr.forEach(function(element){
+    total += element;
+  });
+  return total;
+}
+// max
+function max(arr){
+  var max = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
 }
